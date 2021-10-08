002a9e0c53953e90c07ab7e2a51814d554df4798
Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-10-08 14:05:23 +0200

    Creo el array de los meses

---
+++ Ejercicio3/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $meses = array(
+        1 => array('Enero', 31),
+        2 => array('Febrero', 28),
+        3 => array('Marzo', 31),
+        4 => array('Abril', 31),
+        5 => array('Mayo', 31),
+        6 => array('Junio', 31),
+        7 => array('Julio', 31),
+        8 => array('Agosto', 31),
+        9 => array('Septiembre', 31),
+        10 => array('Octubre', 31),
+        11 => array('Noviembre', 31),
+        12 => array('Diciembre', 31)
+    );
+
+?>
