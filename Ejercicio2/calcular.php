820c76733a203b2e28eac6c53b41458e52d487b2
Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-10-08 13:40:16 +0200

    V1

---
+++ Ejercicio2/calcular.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Calcular{
+
+        function sumar($num1, $num2){
+            return $num1 + $num2;
+        }
+
+        function restar($num1, $num2){
+            return $num1 - $num2;
+        }
+
+        function multiplicar($num1, $num2){
+            return $num1 * $num2;
+        }
+
+        function dividir($num1, $num2){
+            return $num1 / $num2;
+        }
+
+    }
+
+?>
---
+++ Ejercicio2/index.php
@@ -0,0 +1,60 @@
+<?php
+
+    //Importaciones
+    require_once 'calcular.php';
+
+    //Funciones
+    function mostrarOperacion(){
+
+        //Variables
+        $calculadora = new Calcular();
+
+        if(isset($_POST['enviar'])){
+            switch($_POST['operador']){
+                case '+':
+                    $resultado = $calculadora->sumar($_POST['num1'], $_POST['num2']);
+                    break;
+                case '-':
+                    $resultado = $calculadora->restar($_POST['num1'], $_POST['num2']);
+                    break;
+                case '*':
+                    $resultado = $calculadora->multiplicar($_POST['num1'], $_POST['num2']);
+                    break;
+                case '/':
+                    $resultado = $calculadora->dividir($_POST['num1'], $_POST['num2']);
+                    break;
+            }
+            echo 'El resultado de la operación es: '.$resultado;
+        }
+        
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="rtorresgutiérrez.guadalupe@alumnado.fundacionloyola.net" />
+        <title>Cálculos</title>
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+            <input type="text" name="num1" id="" placeholder="Número 1" />
+            <input type="text" name="num2" id="" placeholder="Número 2" />
+            <select name="operador" id="">
+                <option value="" hidden>-- Seleccionar un operador --</option>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input type="submit" value="ENVIAR" name="enviar" />
+            <input type="reset" value="CANCELAR" />
+        </form>
+        <?php
+            mostrarOperacion();
+        ?>
+    </body>
+</html>
