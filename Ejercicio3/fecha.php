87f9fc759c909b480f17b8d272881016fe25974e
Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-10-15 14:00:41 +0200

    Corrección de atributo meses en constructor

--- Ejercicio3/fecha.php
+++ Ejercicio3/fecha.php
@@ -6,22 +6,23 @@
         private int $d;
         private int $m;
         private int $a;
-        private $meses = array(
-            1 => array('Enero', 31),
-            2 => array('Febrero', 28),
-            3 => array('Marzo', 31),
-            4 => array('Abril', 31),
-            5 => array('Mayo', 31),
-            6 => array('Junio', 31),
-            7 => array('Julio', 31),
-            8 => array('Agosto', 31),
-            9 => array('Septiembre', 31),
-            10 => array('Octubre', 31),
-            11 => array('Noviembre', 31),
-            12 => array('Diciembre', 31)
-        );
+        private $meses;
 
         function __construct($fecha){
+            $this->meses = array(
+                1 => array('Enero', 31),
+                2 => array('Febrero', 28),
+                3 => array('Marzo', 31),
+                4 => array('Abril', 31),
+                5 => array('Mayo', 31),
+                6 => array('Junio', 31),
+                7 => array('Julio', 31),
+                8 => array('Agosto', 31),
+                9 => array('Septiembre', 31),
+                10 => array('Octubre', 31),
+                11 => array('Noviembre', 31),
+                12 => array('Diciembre', 31)
+            );
             list($this->d, $this->m, $this->a) = explode('/', $fecha);
         }
         function obtenerFecha(){
